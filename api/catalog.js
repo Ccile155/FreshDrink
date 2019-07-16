a062854898f56f2b79b60e0f3246c4504e6dd7f1
Ccile <cecilisse@gmail.com>
2019-07-16 10:14:57 +0200

    formulaire avec chips mais création non fonctionnelle

--- api/catalog.js
+++ api/catalog.js
@@ -29,12 +29,39 @@ router.get("/liste/:smoothie", async (req, res, next) => {
 
 });
 
-router.post('/add', async (req, res, next) => {
+const {
+    check,
+    validationResult
+} = require('express-validator');
+
+const sanitizeBody = require('express-validator');
+
+router.post('/add', [check('title').not().isEmpty().withMessage("Titre: : donnée manquante"),
+                        check('description').not().isEmpty().withMessage("Description: : donnée manquante")
+                        ],async (req, res, next) => {
+
+    // Validation du formulaire
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        res.status(400).send({
+            errors: errors.array()
+        });
+        return;
+    }
 
     let newSmoothie = new Smoothie();
     newSmoothie.title = req.body.title; //permet de manipuler, populer le nouvel objet de type Smoothie
     newSmoothie.description = req.body.description;
-
+    newSmoothie.picture.name = req.body.pictureName;
+    newSmoothie.picture.path = req.body.picture;
+    newSmoothie.picture.alt = req.body.alt;
+    newSmoothie.features.prepTime = req.body.features.prepTime;
+    newSmoothie.features.cost = req.body.features.cost;
+    newSmoothie.advice = req.body.advice;
+    newSmoothie.ingredients[0].nom = req.body.ingredients;
+    newSmoothie.recipe = req.body.recipe;
+    
     try {
         const smoothie = await newSmoothie.save(); 
         res.send(smoothie); //renvoi du nouveau smoothie avec son nouvel Id, utilisable pour afficher une fiche !
